bff68264b54fe163736711109b3ecbf347b634d2
Rehan Nischal <rehannischal@gmail.com>
2024-12-19 10:39:09 +0530

    updated constants file

--- app/constants.py
+++ app/constants.py
@@ -7,6 +7,6 @@ TEST_SUIT_SUCCESSFUL_DELETION_MESSAGE = "Test Suite deleted successfully"
 TEST_CASE_SUCCESSFUL_DELETION_MESSAGE = "Test Case deleted successfully"
 TEST_RUN_SUCCESSFUL_DELETION_MESSAGE = "Test Run deleted successfully"
 
-TEST_SUIT_NOT_FOUND = "Test Suit not found"
+TEST_SUIT_NOT_FOUND = "Test Suite not found"
 TEST_CASE_NOT_FOUND = "Test Case not found"
 TEST_RUN_NOT_FOUND = "Test Run not found"
